25f42d5e7c0871b82092cdab60990cfc18610dad
Ansh <anshsaxena4190@gmail.com>
2024-09-14 17:25:22 +0530

    feat: parallel execution for pkpMint

--- server/src/pkpMint.ts
+++ server/src/pkpMint.ts
@@ -2,12 +2,10 @@ import { LitNetwork } from "@lit-protocol/constants";
 import { LitContracts } from "@lit-protocol/contracts-sdk";
 import { ethers } from "ethers";
 
-export async function pkpMint(_privateKey: string, _network: LitNetwork) {
-
-    const signer = await getWallet(_privateKey);
+export async function pkpMint(_wallet: ethers.Wallet, _network: LitNetwork) {
 
     const litContracts = new LitContracts({
-        signer: signer,
+        signer: _wallet,
         network: _network,
         debug: false,
     });
@@ -17,15 +15,4 @@ export async function pkpMint(_privateKey: string, _network: LitNetwork) {
     const mintedPkp = await litContracts.pkpNftContractUtils.write.mint();
 
     return mintedPkp.pkp;
-}
-
-
-async function getWallet(_privateKey: string) {
-    const provider = new ethers.providers.JsonRpcProvider(
-        `https://yellowstone-rpc.litprotocol.com/`
-    );
-
-    const wallet = new ethers.Wallet(_privateKey, provider);
-
-    return wallet;
 }
